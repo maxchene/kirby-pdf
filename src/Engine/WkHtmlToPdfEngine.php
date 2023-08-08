d8a4194323921ac011ad5bb37b9a7e8eec04bf6f
Maxime <maxime@kaliel.fr>
2023-08-08 10:43:07 +0200

    pdf options

--- src/Engine/WkHtmlToPdfEngine.php
+++ src/Engine/WkHtmlToPdfEngine.php
@@ -61,8 +61,77 @@ class WkHtmlToPdfEngine extends AsbtractPdfEngine
     protected function getCommand(): string
     {
         $binary = $this->_binary;
+
+        $options = [
+            'quiet' => true,
+            'print-media-type' => true,
+            'orientation' => $this->pdf->getOrientation(),
+            'page-size' => $this->pdf->getPageSize(),
+            'encoding' => 'UTF-8',
+            'title' => $this->pdf->getTitle(),
+            'javascript-delay' => $this->pdf->getDelay(),
+            'margin-bottom' => $this->pdf->getMarginBottom(),
+            'margin-left' => $this->pdf->getMarginLeft(),
+            'margin-right' => $this->pdf->getMarginRight(),
+            'margin-top' => $this->pdf->getMarginTop(),
+        ];
+
         $command = $binary;
+
+        foreach ($options as $key => $value) {
+            if (empty($value) && $value !== 0) {
+                continue;
+            }
+            $command .= $this->parseOptions($key, $value);
+        }
+
         $command .= ' - -';
+        return $command;
+    }
+
+    /**
+     * @param string $key
+     * @param $value
+     * @return string
+     * @throws Exception
+     */
+    protected function parseOptions(string $key, $value): string
+    {
+        $command = '';
+        if (is_array($value)) {
+            if ($key === 'toc') {
+                $command .= ' toc';
+                foreach ($value as $k => $v) {
+                    $command .= $this->parseOptions($k, $v);
+                }
+            } elseif ($key === 'cover') {
+                if (!isset($value['url'])) {
+                    throw new Exception('The url for the cover is missing. Use the "url" index.');
+                }
+                $command .= ' cover ' . escapeshellarg((string)$value['url']);
+                unset($value['url']);
+                foreach ($value as $k => $v) {
+                    $command .= $this->parseOptions($k, $v);
+                }
+            } else {
+                foreach ($value as $k => $v) {
+                    $command .= sprintf(' --%s %s %s', $key, escapeshellarg($k), escapeshellarg((string)$v));
+                }
+            }
+        } elseif ($value === true) {
+            if ($key === 'toc') {
+                $command .= ' toc';
+            } else {
+                $command .= ' --' . $key;
+            }
+        } else {
+            if ($key === 'cover') {
+                $command .= ' cover ' . escapeshellarg((string)$value);
+            } else {
+                $command .= sprintf(' --%s %s', $key, escapeshellarg((string)$value));
+            }
+        }
+
         return $command;
     }
 
--- src/KirbyPdf.php
+++ src/KirbyPdf.php
@@ -18,27 +18,179 @@ class KirbyPdf
 
     protected string $orientation = 'portrait';
 
-    protected string|int|null $marginBottom = null;
+    protected string|int|null $marginBottom = 0;
 
-    protected string|int|null $marginLeft = null;
+    protected string|int|null $marginLeft = 0;
 
-    protected string|int|null $marginRight = null;
+    protected string|int|null $marginRight = 0;
 
-    protected string|int|null $marginTop = null;
+    protected string|int|null $marginTop = 0;
 
     protected string $html = '';
 
+    protected string $title = 'coucou les gens';
+
     protected Page $page;
+
+    protected int|null $delay = null;
 
     public function __construct()
     {
-        $engine = option('maxchene.kirbypdf.engine');
-        if ($engine !== null) {
+        $this->buildOptions();
+    }
 
-
-        }
+    protected function buildOptions(): void
+    {
+        $this->setOrientation(option('maxchene.kirby-pdf.orientation'));
         $this->setEngine(new WkHtmlToPdfEngine($this));
+        $this->setMarginBottom(option('maxchene.kirby-pdf.margin.bottom'));
+        $this->setMarginLeft(option('maxchene.kirby-pdf.margin.left'));
+        $this->setMarginRight(option('maxchene.kirby-pdf.margin.right'));
+        $this->setMarginTop(option('maxchene.kirby-pdf.margin.top'));
 
+    }
+
+    /**
+     * @param string $orientation
+     * @return KirbyPdf
+     */
+    public function setOrientation(string $orientation): KirbyPdf
+    {
+        $this->orientation = $orientation;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getOrientation(): string
+    {
+        return $this->orientation;
+    }
+
+    /**
+     * @param string $title
+     * @return KirbyPdf
+     */
+    public function setTitle(string $title): KirbyPdf
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $pageSize
+     * @return KirbyPdf
+     */
+    public function setPageSize(string $pageSize): KirbyPdf
+    {
+        $this->pageSize = $pageSize;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPageSize(): string
+    {
+        return $this->pageSize;
+    }
+
+    /**
+     * @param int|string|null $marginBottom
+     * @return KirbyPdf
+     */
+    public function setMarginBottom(int|string|null $marginBottom): KirbyPdf
+    {
+        $this->marginBottom = $marginBottom;
+        return $this;
+    }
+
+    /**
+     * @return int|string|null
+     */
+    public function getMarginBottom(): int|string|null
+    {
+        return $this->marginBottom;
+    }
+
+    /**
+     * @param int|string|null $marginLeft
+     * @return KirbyPdf
+     */
+    public function setMarginLeft(int|string|null $marginLeft): KirbyPdf
+    {
+        $this->marginLeft = $marginLeft;
+        return $this;
+    }
+
+    /**
+     * @return int|string|null
+     */
+    public function getMarginLeft(): int|string|null
+    {
+        return $this->marginLeft;
+    }
+
+    /**
+     * @return int|string|null
+     */
+    public function getMarginRight(): int|string|null
+    {
+        return $this->marginRight;
+    }
+
+    /**
+     * @param int|string|null $marginRight
+     */
+    public function setMarginRight(int|string|null $marginRight): KirbyPdf
+    {
+        $this->marginRight = $marginRight;
+        return $this;
+    }
+
+    /**
+     * @return int|string|null
+     */
+    public function getMarginTop(): int|string|null
+    {
+        return $this->marginTop;
+    }
+
+    /**
+     * @param int|string|null $marginTop
+     * @return KirbyPdf
+     */
+    public function setMarginTop(int|string|null $marginTop): KirbyPdf
+    {
+        $this->marginTop = $marginTop;
+        return $this;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getDelay(): ?int
+    {
+        return $this->delay;
+    }
+
+    /**
+     * @param int|null $delay
+     * @return KirbyPdf
+     */
+    public function setDelay(?int $delay): KirbyPdf
+    {
+        $this->delay = $delay;
+        return $this;
     }
 
     public function output(): string
